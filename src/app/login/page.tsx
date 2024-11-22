7efffc2a54a6cbd2ceb95273f5f0150553a33be4
color <jgstringer@gmail.com>
2024-11-21 21:24:00 -0600

    link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,23 @@
 "use client";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import { useTheme } from "../../hooks/theme";
+import Link from "next/link";
 
 export default function Login() {
+    const theme = useTheme();
+    if (!theme) return null;
 
-    const page = "https://dev-45cqnlol3elffk2n.us.auth0.com/authorize?response_type=code&client_id=t6GbvGDxdSIUlkPpCz2yVYAQw0Gm6r3n&redirect_uri=https://api.color.wtf/authenticate";
-    window.location.href = page
-    return null;
+    const bodyClass = `container contentContainer grid content-between min-h-svh`;
+    return (
+        <div className={bodyClass}>
+            <main className="mainContent flex flex-col p-3 md:p-10 text-2xl">
+                <Header />
+                <div>
+                    <Link href="https://dev-45cqnlol3elffk2n.us.auth0.com/authorize?response_type=code&client_id=t6GbvGDxdSIUlkPpCz2yVYAQw0Gm6r3n&redirect_uri=https://api.color.wtf/authenticate" />
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
 }
